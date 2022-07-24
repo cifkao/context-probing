a53a833974f4b5e98a79a92f7bb9bb5b48be2bef
Ondřej Cífka <cifkao@users.noreply.github.com>
2022-07-24 17:14:40 +0200

    preds_to_xents without dask

--- long_dep/scripts/preds_to_xents.py
+++ long_dep/scripts/preds_to_xents.py
@@ -1,30 +1,24 @@
 import argparse
 import glob
-import sys
 
-import dask
-import dask.dataframe as dd
-import dask.diagnostics
-import dask.array as da
 import numpy as np
 import torch
-import torch.functional as F
+from tqdm import tqdm
 
-from .predict_sliding import get_data, get_shard_sizes
+from .predict_sliding import get_data
 
 
-def _get_saved_shape_and_dtype(path):
+def _get_saved_shape(path):
     arr = np.load(path, mmap_mode="r")
-    return arr.shape, arr.dtype
-
-
+    return arr.shape
 
 
 def compute_xents(logits, labels):
     logits = torch.as_tensor(logits, dtype=torch.float32)
     labels = torch.as_tensor(labels)
     loss_fn = torch.nn.CrossEntropyLoss(reduction="none")
-    xents = loss_fn(logits.view(-1, logits.shape[-1]), labels.view(-1)).reshape(logits.shape[:2])
+    xents = loss_fn(logits.reshape(-1, logits.shape[-1]), labels.reshape(-1))
+    xents = xents.reshape(logits.shape[:2])
     return xents.numpy()
 
 
@@ -33,48 +27,30 @@ def main():
     parser.add_argument("data_path", type=str)
     parser.add_argument("input_path_pattern", type=str)
     parser.add_argument("output_path", type=str)
-    parser.add_argument("--num-shards", type=int, default=None)
     args = parser.parse_args()
 
     shard_paths = sorted(glob.glob(args.input_path_pattern))
-    (_, window_len, vocab_size), dtype_in = _get_saved_shape_and_dtype(shard_paths[0])
-    dataset = get_data(args.data_path, window_len=window_len, columns=["input_ids"])
-    shard_sizes = get_shard_sizes(dataset, args.num_shards or len(shard_paths))
-
-    if len(shard_sizes) > len(shard_paths):
-        print(
-            f"Found {len(shard_paths)} shards instead of {len(shard_sizes)}. "
-            "Truncating dataset.",
-            file=sys.stderr
-        )
-        shard_sizes = shard_sizes[:len(shard_paths)]
-        dataset = dataset.select(range(sum(shard_sizes)))
-
-    logits = da.concatenate([
-        da.from_delayed(
-            dask.delayed(np.load)(path, mmap_mode="r"),
-            shape=(size, window_len, vocab_size), 
-            dtype=dtype_in
-        )
-        for path, size in zip(shard_paths, shard_sizes)
-    ])
-    logits = logits[:, :-1]
-
-    labels = da.from_array(
-        np.asarray(dataset["input_ids"])[:len(logits), 1:],
-        chunks=logits.chunks[:-1]
+    dataset = get_data(
+        args.data_path,
+        window_len=_get_saved_shape(shard_paths[0])[1],
+        columns=["input_ids"]
     )
 
-    xents = da.map_blocks(compute_xents, logits, labels[..., None], drop_axis=[-1], dtype=np.float32)
+    inputs = np.asarray(dataset["input_ids"])
 
-    print("Logits:", logits, file=sys.stderr)
-    print("Labels:", labels, file=sys.stderr)
-    print("Xents: ", xents, file=sys.stderr)
+    start_idx = 0
+    xents = []
+    for path in tqdm(shard_paths):
+        logits = np.load(path)[:, :-1]
+        end_idx = start_idx + len(logits)
+        labels = inputs[start_idx:end_idx, 1:]
 
-    with dask.diagnostics.ProgressBar():
-        xents_np = xents.compute()
+        xents.append(compute_xents(logits, labels))
 
-    np.save(args.output_path, xents_np)
+        start_idx = end_idx
+    xents = np.concatenate(xents, axis=0)
+
+    np.save(args.output_path, xents)
 
 
 if __name__ == "__main__":
