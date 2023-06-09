56eb3bb2c4dda8d0818f88a72b079de0144e0a84
Ondřej Cífka <cifkao@users.noreply.github.com>
2023-06-10 01:21:20 +0200

    Use iframe-resizer

--- www/src/index.tsx
+++ www/src/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import iFrameResize from "iframe-resizer/js/iframeResizer";
 
 import { App } from "./app";
 
 const app = document.getElementById("app") as HTMLElement;
 const root = createRoot(app);
 root.render(<App />);
+
+iFrameResize({}, ".demo-iframe");
